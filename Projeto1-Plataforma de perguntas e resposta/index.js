8891b4fe178d95cf0743f14c04d9e7947136d3fe
Izabella B Lopes <izabella.bl@hotmail.com>
2022-01-24 13:51:25 -0300

    formulario de pergunta

--- Projeto1-Plataforma de perguntas e resposta/index.js
+++ Projeto1-Plataforma de perguntas e resposta/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 
 app.set('view engine','ejs'); 
 app.use(express.static('public')); 
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{ 
     res.render("index");
@@ -14,6 +16,11 @@ app.get("/perguntar",(req,res)=>{
    res.render("perguntar");
 });
 
+app.post("/salvarpergunta",(req,res)=>{
+   var titulo = req.body.titulo;
+   var descricao = req.body.descricao;
+   res.send("Formulario recebido!"+titulo+"-"+descricao);
+})
 app.listen(8081,()=>{
     console.log("app rodando")
 });
